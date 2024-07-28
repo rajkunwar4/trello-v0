102dff7ff57ce53dfb76b62714268c87ca037b86
rajkunwar4 <40rajkunwar@gmail.com>
2024-07-28 14:14:06 +0530

    Image config

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="border">
+      <Image
+        src="https://links.papareact.com/c2cdd5"
+        alt="logo"
+        width={300}
+        height={140}
+        className="w-44 md:w-56 pb-10 md:pb-0 object contain"
+      />
+    </header>
+  );
 }
