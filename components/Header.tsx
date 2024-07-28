653510210e4fa24cd69295d0b74658a8930112ce
rajkunwar4 <40rajkunwar@gmail.com>
2024-07-28 23:24:48 +0530

    navbar + gradient

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,6 +9,7 @@ export default function Header() {
   return (
     <header className="">
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 ">
+        <div className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-300 to-blue-200 z-[-1]  filter blur-3xl" />
         <Image
           src="https://links.papareact.com/c2cdd5"
           alt="logo"
@@ -35,9 +36,13 @@ export default function Header() {
 
           {/* user avatar */}
           <Avatar name="r k" round color="#0055d1" size="50" />
-
-          {/* <UserCircleIcon className="size-5" /> */}
         </div>
+      </div>
+      <div className="flex justify-center items-center px-5  md:py-5 font-light py-2">
+        <p className="flex items-center justify-center pr-5 text-sm shadow-xl rounded italic  max-w-3xl text-[#0055d1]/60 p-4 bg-white">
+          <UserCircleIcon className="w-10 h-10 inline-block text-[#0055d1]" />
+          GPT is summarising your tasks...
+        </p>
       </div>
     </header>
   );
